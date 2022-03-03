d96f583c444661b9a6054655687ad5dfba0f7aa1
Phillip Zastrow <zastrow.phillip@gmail.com>
2022-03-03 18:06:49 +0100

    led test

---
+++ Arduino_led_1/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+/*
+  Simple Blink sketch
+  simple-blink.cpp
+  Use for PlatformIO demo
+
+  From original Arduino Blink Sketch
+  https://www.arduino.cc/en/Tutorial/Blink
+  
+  DroneBot Workshop 2021
+  https://dronebotworkshop.com
+*/
+
+// Set LED_BUILTIN if undefined or not pin 13
+int led = 13;
+
+void setup()
+{
+  // Initialize LED pin as an output.
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  // Set the LED HIGH
+  digitalWrite(led, HIGH);
+
+  // Wait for a second
+  delay(1000);
+
+  // Set the LED LOW
+  digitalWrite(led, LOW);
+
+   // Wait for a second
+  delay(1000);
+}
